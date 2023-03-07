45f11ed47eb620f5006fce7cfd52410df0f81e10
gbochmann <gbochmann@salesforce.com>
2023-03-07 09:03:00 +0100

    Initial Commit

---
+++ frances/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  frances
+//
+//  Created by Gerome Bochmann on 3/7/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ frances/francesApp.swift
@@ -0,0 +1,17 @@
+//
+//  francesApp.swift
+//  frances
+//
+//  Created by Gerome Bochmann on 3/7/23.
+//
+
+import SwiftUI
+
+@main
+struct francesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
