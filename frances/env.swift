d65ea4dfa818ecf634f458919b832ea1aa6c2058
gbochmann <gbochmann@salesforce.com>
2023-03-15 22:49:23 +0100

    fleshed out environment

--- frances/env.swift
+++ frances/env.swift
@@ -9,6 +9,40 @@ import Foundation
 
 typealias ArithmeticFn = ([Node]) -> Node
 
+enum EnvironmentError: Error {
+    case NotFound(String)
+}
+
+class Env {
+    let outer: Env?
+    var table: [String:Node]
+    
+    init(outer: Env?) {
+        self.outer = outer
+        self.table = [:]
+    }
+    
+    func set(_ key: String, value: Node) {
+        self.table[key] = value
+    }
+    
+    func get(_ key: String) throws -> Node {
+        guard let env = find(key) else {
+            throw EnvironmentError.NotFound("Could not locate \(key).")
+        }
+        
+        return try env.get(key)
+    }
+    
+    func find(_ key: String) -> Env? {
+        if table[key] != nil {
+            return self
+        } else {
+            return outer?.find(key)
+        }
+    }
+}
+
 func calc( fn: @escaping (Int, Int) -> Int) -> ArithmeticFn {
     func calcWrapper(args: [Node]) -> Node {
         guard case let .Number(first) = args.first else { return .Number(0) }
